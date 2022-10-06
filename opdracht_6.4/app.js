07131ccb04db15ef218c180fe3c90ea813f87fcb
Dinandvanwieringen <112871243+Dinandvanwieringen@users.noreply.github.com>
2022-10-06 12:35:19 +0200

    Add files via upload

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,17 @@
+function calculate(num, num2) {
+    let answer = num + num2;
+    console.log(answer)
+}
+calculate(567373, 50914);
+
+function multiplyByFive(num3) {
+    let answer2 = num3 * 5
+    console.log(answer2)
+}
+multiplyByFive(13)
+
+function calculateMinutes(num4) {
+    let answer3 = num4 / 60
+    console.log(answer3)
+}
+calculateMinutes(5400)
